65be9f0215d7300a8f179af4aad4c23e2988787d
Hans Peter Hagblom <HansPeter.Hagblom@cybercom.com>
2016-06-17 11:10:27 +0200

    No longer required to pass in a ConfigParser object

--- awslambdadecorators/httpxml_to_s3.py
+++ awslambdadecorators/httpxml_to_s3.py
@@ -3,6 +3,7 @@ import uuid
 import httplib
 import logging
 import json
+import urlparse
 
 import boto.s3 as s3
 
@@ -14,16 +15,44 @@ logger = logging.getLogger('httpxml_to_s3')
 logger.setLevel(logging.INFO)
 
 
-def httpxml_to_s3(config1, xml_preprocessor=lambda s: s):
+def httpxml_to_s3(http_url,
+                  s3_url,
+                  region='eu-west-1',
+                  profile_name=None,
+                  xml_preprocessor=lambda s: s):
     """
     Decorator function configuring function
-    config1 - Is a ConfigParser parser object containing information on which
-              URL to poll and the location on s3 where to store the synced
-              files.
     xml_preprocessor - If some preprocessing needs to be done on the xml as
                        a string a lambda can be sent in. Defaults to the
                        identity function
     """
+
+    parsed_url = urlparse.urlparse(http_url)
+
+    parsed_s3_url = urlparse.urlparse(s3_url);
+
+    bucket_name = parsed_s3_url.hostname;
+    key_prefix = parsed_s3_url.path;
+    if key_prefix[0] == '/':
+        key_prefix = key_prefix[1:]
+    if key_prefix[-1] != '/':
+        key_prefix = key_prefix + '/'
+
+
+    def get_page():
+        logger.info("Syncing feed on %s",
+                    http_url)
+        if parsed_url.scheme == 'https':
+            conn = httplib.HTTPSConnection(parsed_url.hostname, parsed_url.port)
+        elif parsed_url.scheme == 'http':
+            conn = httplib.HTTPConnection(parsed_url.hostname, parsed_url.port)
+        conn.request('GET', parsed_url.path);
+        resp = conn.getresponse()
+        response_string = xml_preprocessor(resp.read())
+        resp.close()
+        conn.close()
+        return ET.fromstring(response_string)
+
     def inner(fn_inner):
         """
         Decorator function function sent in should be having signature
@@ -35,20 +64,14 @@ def httpxml_to_s3(config1, xml_preprocessor=lambda s: s):
             """
             The AWS Lambda Entry Point
             """
-            s3conn = s3.connect_to_region('eu-west-1', profile_name='personal')
-            bucket_name = config1.get('httpxml_to_s3', 'bucket')
-            key_prefix = config1.get('httpxml_to_s3', 'key_prefix')
-
-            input_xml = get_page(config1.get('httpxml_to_s3', 'host'),
-                                 config1.getint('httpxml_to_s3', 'port'),
-                                 config1.get('httpxml_to_s3','path'), xml_preprocessor)
-
+            s3conn = s3.connect_to_region(region, profile_name=profile_name)
             bucket = s3conn.get_bucket(bucket_name)
 
             # Use a map to track keys that are no longer in the feed, used for deletion
             remaining_keys = { key.name : True for key in bucket.list(prefix=key_prefix)}
 
             logger.debug("Existing keys in bucket\n%s", '\n'.join(remaining_keys));
+            input_xml = get_page()
 
             for id, json_data in fn_inner(input_xml):
                 key_name = key_prefix + str(uuid.uuid5(uuid.NAMESPACE_URL, id.encode('utf-8')))
@@ -83,17 +106,4 @@ def httpxml_to_s3(config1, xml_preprocessor=lambda s: s):
         return handler
 
     return inner
-
-def get_page(host, port, path, xml_preprocessor=lambda s: s):
-    logger.info("Syncing feed on https://%s:%s%s",
-                host,
-                port,
-                path)
-    conn = httplib.HTTPSConnection(host, port)
-    conn.request('GET', path);
-    resp = conn.getresponse()
-    response_string = xml_preprocessor(resp.read())
-    resp.close()
-    conn.close()
-    return ET.fromstring(response_string)
 
